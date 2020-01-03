9265286f60486a2d9f64e13a9ddfca8a2945ad62
pratik.einfach <pratik@einfachanalytics.com>
2020-01-03 16:32:00 +0530

    Function Constructor

---
+++ script.js
@@ -0,0 +1,18 @@
+//fucntion constructer
+
+var Presion= function(name,age){
+this.name=name;
+this.age=age;
+this.logName=function(){
+console.log(this.name);
+}
+}
+
+Presion.prototype.logAge=function(){
+console.log(this.age);
+}
+
+var pratik=new Presion('pratik kumar',29);
+
+pratik.logName();
+pratik.logAge();
