71e861939f2f5e5ab301e94ef217a3f2f69c9a5c
pratik.einfach <pratik@einfachanalytics.com>
2020-01-03 17:09:22 +0530

    Callback function

---
+++ script-3-Callback function.js
@@ -0,0 +1,36 @@
+// Callback function
+
+
+var years=[1908,1947,2008,2001,1999,1888];
+
+function arrCal(arr,fun) {
+	var returnArr=[];
+
+	for(var i=0; i <arr.length;i++){
+		returnArr.push(fun(arr[i]));
+	}
+	return returnArr;
+}
+
+function age(year) {
+	return 2020-year;
+}
+
+ages=arrCal(years,age);
+
+console.log(ages);
+
+
+function canVote(age) {
+ 
+	if(age>18){
+		return true;
+	}else{
+		return -1;
+	}
+
+}
+
+var voters= arrCal(ages,canVote);
+
+console.log(voters);
